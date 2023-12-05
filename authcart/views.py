efe9457bc23c19197e1c5a06446f12c1b24872c6
fitbouncer <bhuinjohn@gmail.com>
2023-12-05 23:51:46 +0530

    registration

--- authcart/views.py
+++ authcart/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 def register(request):
+    if (request.method =="post"):
+        print('hrre')
+        email = request.POST['email']
+        name = request.POST['name']
+        password = request.POST['password']
+        confirmPassword = request.POST['confirmPassword']
+        if password != confirmPassword:
+            return HttpResponse('password dont match')
+
+        
+        try:
+            if User.objects.get(username=name):
+                 return HttpResponse('user exist')
+        except Exception as identifier:
+            return HttpResponse(identifier)
+
+        user = User.objects.create_user(name, email, password)
+        user.save()
+        return HttpResponse('user created')
+    print('th')
     return render(request, "auth/register.html")
 def login(request):
     return render(request, "auth/login.html")
